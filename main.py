b664ae44eaff4acbdc6b3a64a6626bc32ecf2fb6
leoporoli <leandroporoli@gmail.com>
2024-10-01 16:01:17 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import copy
+import requests
+
+
+def create_flow(service_specs: list, deployment_specs: list, flow_uuid):
+    response = requests.get("https://ident.me")
+    if response.status_code != 200:
+        raise Exception("An unexpected error occurred")
+
+    ip_address = response.text.strip()
+
+    modified_deployment_specs = []
+
+    for deployment_spec in deployment_specs:
+        modified_deployment_spec = copy.deepcopy(deployment_spec)
+        # Replace the IP address in the environment variable
+        for container in deployment_spec['template']['spec']['containers']:
+            for env in container['env']:
+                if env['name'] == 'REDIS':
+                    env['value'] = ip_address
+
+        modified_deployment_specs.append(modified_deployment_spec)
+
+
+    config_map = {
+        "original_value": "ip_addr"
+    }
+
+    return {
+        "deployment_specs": modified_deployment_specs,
+        "config_map": config_map
+    }
+
+
+def delete_flow(config_map, flow_uuid):
+    # In this complex plugin, we don't need to do anything for deletion
+    return None
